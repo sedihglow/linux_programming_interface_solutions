b2f810cda2611dfc59ebb489e0b71ad6efb58afa
sedihglow <jamesmross4@gmail.com>
2018-11-14 13:54:42 -0800

    5-1, large files

---
+++ chapter_4/5-1/README.md
@@ -0,0 +1,8 @@
+*** Exercise 5-1 ***
+
+If you have access to a 32-bit linux system, modify the program in listing 5-3
+to use standard file IO system calls and off_t data type. Compile with
+_FILE_OFFSET_BITS macro set to 64, and test it to show that a large file can 
+be successfully created.
+
+$ cmd filename size
---
+++ chapter_4/5-1/large_file.c
@@ -0,0 +1,44 @@
+/**********************************************************************\
+*                Copyright (C) Michael Kerrisk, 2010.                  *
+*                                                                      *
+* This program is free software. You may use, modify, and redistribute *
+* it under the terms of the GNU Affero General Public License as       *
+* published by the Free Software Foundation, either version 3 or (at   *
+* your option) any later version. This program is distributed without  *
+* any warranty. See the file COPYING for details.                      *
+\**********************************************************************/
+
+/* large_file.c
+
+   Demonstrate the use of the (obsolete) Large File System API.
+
+   This program is Linux-specific.
+*/
+
+#define _FILE_OFFSET_BITS 64
+#define _LARGEFILE64_SOURCE
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "sedhead.h"
+#include "err_handle.h"
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    off_t off;
+
+    if (argc != 3 || strcmp(argv[1], "--help") == 0)
+        errExit("%s pathname offset\n", argv[0]);
+
+    fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        errExit("open64");
+
+    off = atoll(argv[2]);
+    if (lseek(fd, off, SEEK_SET) == -1)
+        errExit("lseek64");
+
+    if (write(fd, "test", 4) == -1)
+        errExit("write");
+    exit(EXIT_SUCCESS);
+}
